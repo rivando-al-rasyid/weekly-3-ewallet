f559863168c407ca0a550e980ae02f4b54a7fd4a
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-03-15 07:19:37 +0700

    feat : linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,45 +1,21 @@
-import js from "@eslint/js";
-import globals from "globals";
-import stylistic from "@stylistic/eslint-plugin";
 import { defineConfig } from "eslint/config";
+import html from "@html-eslint/eslint-plugin";
 
 export default defineConfig([
-  js.configs.recommended,
+    {
+        files: ["**/*.html"],
+        plugins: {
+            html,
+        },
+        // When using the recommended rules (or "html/all" for all rules)
+        extends: ["html/recommended"],
+        language: "html/html",
+        rules: {
+            "html/no-duplicate-class": "error",
+            "html/element-newline": "error",
+            "html/indent": ["error", 4]
 
-  {
-    files: ["**/*.{js,mjs,cjs}"],
 
-    plugins: {
-      "@stylistic": stylistic,
-    },
-
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
-    },
-
-    rules: {
-      /* ESLint core rules */
-      "no-unused-vars": "warn",
-      "no-console": "off",
-      "no-debugger": "warn",
-      "eqeqeq": ["error", "always"],
-      "curly": ["error", "all"],
-      "no-var": "error",
-      "prefer-const": "error",
-
-      /* Stylistic rules */
-      "@stylistic/indent": ["error", 2],
-      "@stylistic/quotes": ["error", "single"],
-      "@stylistic/semi": ["error", "always"],
-      "@stylistic/comma-dangle": ["error", "always-multiline"],
-      "@stylistic/object-curly-spacing": ["error", "always"],
-      "@stylistic/array-bracket-spacing": ["error", "never"],
-      "@stylistic/space-before-function-paren": ["error", "never"],
-    },
-  },
+        }
+    }
 ]);
